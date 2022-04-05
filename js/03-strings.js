7f07bc06356f7836a30df01c3b3a6fb1822e101e
dileta.nenene@gmail.com <dileta.nenene@gmail.com>
2022-04-05 18:03:21 +0300

    string

---
+++ js/03-strings.js
@@ -0,0 +1,22 @@
+/*
+STRING
+
+kabutes:
+-viengubos
+-dvigubos
+*/
+
+const name = 'Petras';
+console.log(name);
+
+const lastname = "Petraitis";
+console.log(lastname);
+
+const viengubaKabute = "viengubos(')";
+console.log(viengubaKabute);
+
+const dvigubaKabute = 'dvigubos(")';
+console.log(dvigubaKabute);
+
+const dvigubaKabute12 = 'Cia yra vienguba (\') ir dviguba (\") kabutės.'; 
+console.log(dvigubaKabute12);
