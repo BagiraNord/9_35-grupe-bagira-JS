1dee7aaca8b455b0d0ada8665d98389e8fa30d9a
dileta.nenene@gmail.com <dileta.nenene@gmail.com>
2022-04-08 10:54:46 +0300

    5 week final task submit

---
+++ js/16 - object.js
@@ -0,0 +1,22 @@
+/*
+OBJECT
+*/
+
+/*
+Mokiniai (array): 
+     mokinys (???);
+        - varda (string),
+        - pazymius (array),
+        - kontaktini asmeni (???),
+            - varda (string),
+            - telefono nr (number)
+*/
+
+    const students = []
+    const marks = []
+    const contactName = []
+    const contactPhone = []
+    
+    const studentIndex = 0;
+    
+    console.log('Vardas:', students[studentIndex]);
